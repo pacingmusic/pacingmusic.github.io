12b8a6ba2622741d6e8b60c32cfedbc837620040
kaitlinmctigue <kaitlinmctigue@gmail.com>
2022-04-09 20:58:49 -0400

    hatemail desc

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # pacingmusic.github.io
+
+
+code on source
+
+npm run deploy to deploy to gh-pages
--- src/Homepage.js
+++ src/Homepage.js
@@ -10,6 +10,7 @@ import pandora from './images/pandora.png';
 import instagram from './images/instagram.png';
 import youtube from './images/youtube.png';
 import twitter from './images/twitter.png';
+import hatemail from './images/cover_hatemail-front--webres.jpeg';
 
 function Homepage() {
     return (
@@ -30,7 +31,7 @@ function Homepage() {
                             <p>
                                 Pacing is a project where I get to try on a bunch of different characters and see what they have
                                 to say. Most of them are profane, self-obsessed, shameless, unreliable narrators… I guess a
-                                little bit of the real me always manages to slip in...
+                                little bit of the real me always manages to slip in.
                             </p>
                             <p>
                                 My writing has been described as “tongue firmly planted in cheek” (Surviving the Golden Age, 2022), an “off-kilter and wonderfully weird world” (CLOUT, 2022), and “I don’t get it” (multiple sources).
@@ -51,6 +52,63 @@ function Homepage() {
                     <BigLink name='apple music' img={apple} link={'https://music.apple.com/us/artist/pacing/1493615757'} />
                     <BigLink name='soundcloud' img={soundcloud} link={'https://soundcloud.com/pacingmusic'} />
                     <BigLink name='pandora' img={pandora} link={'https://www.pandora.com/playlist/PL:126724214806811588:342066879'}/>
+                </div>
+            </WindowBlock>
+
+            {/*Intro*/}
+            <WindowBlock header={'hatemail'}>
+                <div className={'message'}>
+                    <div className={'message_sidebar'}>
+                        <div className={'message_sidebar_item'}>
+                            hatemail ->
+                        </div>
+                        <div className={'message_sidebar_item'}>
+                            inbox
+                        </div>
+                        <div className={'message_sidebar_item'}>
+                            sent
+                        </div>
+                        <div className={'message_sidebar_item'}>
+                            drafts
+                        </div>
+                        <div className={'message_sidebar_item'}>
+                            spam
+                        </div>
+                        <div className={'message_sidebar_item'}>
+                            love letters
+                        </div>
+                    </div>
+                    <div className={'message_body'}>
+                        <div className={'message_body_subject'}>
+                            Subject: <span>About hatemail</span>
+                        </div>
+                        <div className={'message_body_email'}>
+                            <p>
+                                I like to deal with my anxiety in the very healthy manner of repeatedly telling myself what an annoying idiot jerk I am. Every once in a while it works because I realize how ridiculous I sound.
+                            </p>
+                            <p>
+                                My theory is that fully succumbing to the mean voices in your lizard brain can be a helpful form of immersion therapy, in small doses. I tested this theory out on the characters of this record, from a macho mafia-aspirant plotting to kill Tony Soprano, to a FOMO-plagued socialite having a panic attack, to - well, myself. These characters dramatically lash out at themselves and others, with mixed results.
+                            </p>
+                            <p>
+                                The mixtape starts off bold and angry and gradually runs out of steam until the narrator is left standing alone, with no choice but to make friends with themself.
+                            </p>
+                            {/*<p>*/}
+                                {/*“hatemail” was recorded with love in my home studio (“the fortress of songitude") with the help of my ex-music-theorist husband Ben and our two cats, who are professional sound engineers. The result is an indie-pop soundscape of anxiety, with ubiquitous foley and a cruel-but-angelic backing vocals of those pesky voices in your head.*/}
+                            {/*</p>*/}
+                            <p>
+                                I like to think this is the mixtape you would make for the person you hate most. &lt;3
+                            </p>
+
+                            <div className={'message_body_email_attachments'}>
+                                Attachments:
+                                <div className={'links'}>
+                                    <BigLink name='preorder' img={bandcamp} link={'https://pacingmusic.bandcamp.com/album/hatemail-2'} />
+                                    <BigLink name='presave' img={spotify} link={'https://distrokid.com/hyperfollow/pacing/hatemail'} />
+                                    <img className={'cover'} src={hatemail} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </WindowBlock>
 
